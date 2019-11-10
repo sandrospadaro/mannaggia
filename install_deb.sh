7d976a209981e57999efe1a374b0952f15c5196c
Sandro Spadaro <sandro.spadaro@gmail.com>
2019-11-10 15:13:56 +0100

    debug

--- install_deb.sh
+++ install_deb.sh
@@ -4,7 +4,7 @@ apt update && \
 	apt install python3 -y && \
 	mkdir -p /etc/mannaggia.d && \
 	cp src/mannaggia /usr/sbin/mannaggia && \
-	ln /usr/sbin/mannaggia /usr/sbin/cowdamn && \
+	ln -f /usr/sbin/mannaggia /usr/sbin/cowdamn && \
 	cp src/mannaggia.dat  /etc/mannaggia.d/mannaggia.dat && \
 	cp src/mannaggia.cow  /etc/mannaggia.d/mannaggia.cow && \
 	chmod +x /usr/sbin/mannaggia && \
